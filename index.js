d5d0998fab5aa6a1bbe54f5dfe86129f1eeb24a4
Utsojet <utsojetpaticor.cse9.bu@gmail.com>
2025-05-27 22:41:56 +0600

    final commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let selectedSeats = [];
 function addToCart(seat) {
     const seatClass = "Economy";
     const price = 500;
-    const seatButton = document.getElementById(seat + "-seat");
+    const seatButton = document.getElementById(seat + " seat");
     const cart = document.getElementById("cart");
 
     const index = selectedSeats.indexOf(seat);
@@ -44,7 +44,7 @@ function addToCart(seat) {
     const totalPrice = selectedSeatsCount * price;
     totalPriceElement.innerText = `BDT ${totalPrice}`;
 
-    const leftNumbersOfSeats = document.getElementById("leftNumbersOfSeats");
+    const leftNumbersOfSeats = document.getElementById("leftNumbersOfSeat");
     const leftSeat = 40 - selectedSeatsCount;
     leftNumbersOfSeats.innerText = `${leftSeat} Seats Left`;
 
@@ -54,15 +54,53 @@ function addToCart(seat) {
 
 function cuponHandler() {
     const cuponElement = document.getElementById("cupon");
-    const cuponValue = cuponElement.value.trim();
+    const cuponValue = cuponElement.value;
+    const Button = document.getElementById("Button");
+    
     const grandTotalElement = document.getElementById("grandTotal");
-    const totalPriceElement = document.getElementById("totalPrice");
 
-    let totalPrice = parseInt(totalPriceElement.innerText.replace("BDT ", ""));
-
-    if (cuponValue === "NEW15") {
-        
-    } else {
-        grandTotalElement.innerText = `BDT ${totalPrice}`;
+    if( cuponValue === "NEW15"){
+        Button.removeAttribute("disabled");
+        const grandTotal = selectedSeatsCount * 500 -(selectedSeatsCount * 500 * 0.15).toFixed(2);
+        grandTotalElement.innerText = `BDT ${grandTotal}`;
+        const cuponContainer = document.getElementById("cuponContainer");
+        cuponContainer.classList.add("hidden");
+        const discountContainer = document.getElementById("discountContainer");
+        discountContainer.style.display = "flex";
+        const discount = document.getElementById("discount");
+        discount.innerText = `BDT ${selectedSeatsCount * 500 * 0.15}`;
+    }else if (cuponValue === "Couple 20") {
+        Button.removeAttribute("disabled");
+        const grandTotal = selectedSeatsCount * 500 - (selectedSeatsCount * 500 * 0.20).toFixed(2);
+        grandTotalElement.innerText = `BDT ${grandTotal}`;
+        const cuponContainer = document.getElementById("cuponContainer");
+        cuponContainer.classList.add("hidden");
+        const discountContainer = document.getElementById("discountContainer");
+        discountContainer.style.display = "flex";
+        const discount = document.getElementById("discount");
+        discount.innerText = `BDT ${selectedSeatsCount * 500 * 0.20}`;
+    }else{
+        // Button.setAttribute("disabled");
+        alert("Invalid cupon code");
     }
+
+}
+function testHandler() {
+  const numberElement = document.getElementById("number");
+  const number = parseFloat(numberElement.value);
+
+  const finalSubmitButton = document.getElementById("finalSubmit");
+  const modal = document.getElementById("modal");
+  const main = document.getElementById("main");
+  if (!isNaN(number) && number > 0 && selectedSeatsCount > 0) {
+    // finalSubmitButton.disabled = false;
+    finalSubmitButton.removeAttribute("disabled");
+    modal.style.display = "block";
+    main.style.display = "none";
+    console.log(number);
+  } else {
+    // finalSubmitButton.disabled = true;
+    finalSubmitButton.setAttribute("disabled");
+    console.log("Invalid number");
+  }
 }
